17211ad70aa34766497ddde57836a55eeaf8b68c
Tomáš Syrový <syrovy.tom@gmail.com>
2015-08-17 15:00:53 +0200

    Zprovoznění cookies.

--- assets-dev/js/main.js
+++ assets-dev/js/main.js
@@ -4,14 +4,38 @@ $(document).ready(function() {
 	/**
 	 * EU cookie info
 	 */
-    $('.cookies-info').delay(1000).slideDown(500);
-
+    if(readCookie("eu-cookies-info") != "1"){
+        $('.cookies-info').delay(1000).slideDown(500);
+    }
+    
     $('.cookies-info button').click(function() {
-        var date = new Date();
-        date.setFullYear(date.getFullYear() + 10);
-        document.cookie = 'cookie-agreed=1; path=/; expires=' + date.toGMTString();
-        $('.cookie-info').slideUp();
+        createCookie("eu-cookies-info", "1", 30);
+        $('.cookies-info').slideUp();
     });
 
-
 });
+
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime()+(days*24*60*60*1000));
+        var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name,"",-1);
+}
