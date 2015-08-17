d1a701d35c711c38e80797ea72263a3569edf933
Jiří Šebek <jiri@query.cz>
2015-08-17 18:23:52 +0200

    Úprava gruntfile a cest

--- gruntfile.js
+++ gruntfile.js
@@ -3,45 +3,6 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 
 
-		// Pospojuj JS
-		concat: {
-			js: {
-				options: {
-					separator: ';'
-				},
-				src: [
-
-
-				// Cookies info
-				'js/cookies-info.js',
-
-
-				// Main.js
-				'js/main.js'
-
-
-				],
-				dest: '../assets/js/main.min.js'
-			}
-		},
-
-
-
-
-
-		// Minifikuj JS
-		uglify: {
-			options: {
-				mangle: false
-			},
-			js: {
-				files: {
-					'../assets/js/main.min.js': ['../assets/js/main.min.js']
-				}
-			}
-		},
-
-
 
 
 
@@ -49,7 +10,7 @@ module.exports = function(grunt) {
 		less: {
 			style: {
 				files: {
-					"../assets/css/style.min.css": "less/style.less"
+					"css/style.min.css": "less/style.less"
 				},
 				options: {
 					compress: true,
@@ -57,7 +18,7 @@ module.exports = function(grunt) {
 					yuicompress: true,
 
 					sourceMap: false, // Nezapomenout přeponout i v Autoprefixeru!
-					sourceMapFilename: '../assets/css/style.min.css.map', // where file is generated and located
+					sourceMapFilename: 'css/style.min.css.map', // where file is generated and located
 					sourceMapURL: 'style.min.css.map', // the complete url and filename put in the compiled css file
 					sourceMapBasepath: '', // Sets sourcemap base path, defaults to current working directory.
 					sourceMapRootpath: '../' // adds this path onto the sourcemap filename and less file paths
@@ -75,8 +36,8 @@ module.exports = function(grunt) {
 				map: false
 			},
 			file: {
-				src: '../assets/css/style.min.css',
-				dest: '../assets/css/style.min.css'
+				src: 'css/style.min.css',
+				dest: 'css/style.min.css'
 			}
 		},
 
@@ -91,7 +52,7 @@ module.exports = function(grunt) {
 			},
 			jquery: {
 				files: {
-					'../assets/js/jquery.min.js': 'jquery/dist/jquery.min.js'
+					'js/jquery.min.js': 'jquery/dist/jquery.min.js'
 				}
 			}
 		},
@@ -125,20 +86,12 @@ module.exports = function(grunt) {
 			configFiles: {
 				files: ['gruntfile.js'] // Při aktualizace gruntfile.js jej znovu načti (netřeba watch exitovat a znovu spouštět)
 			},
-			js: {
-				files: ['js/*.js'],
-				tasks: ['concat:js', 'uglify:js', 'notify:notify_js'],
-				options: {
-					spawn: false,
-					livereload: true
-				}
-			},
 			less: {
 				files: ['less/**/*.less'],
 				tasks: ['less:style', 'autoprefixer:file', 'notify:notify_less'],
 				options: {
 					spawn: false,
-					livereload: true
+					livereload: false
 				}
 			}
 		},
@@ -151,8 +104,6 @@ module.exports = function(grunt) {
 
 
 	// Naloaduj tasky
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-bowercopy');
